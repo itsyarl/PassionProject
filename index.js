87420fcdccb39887b15ac70d363595f1c5860062
itsyarl <yarl.van.onckelen@gmail.com>
2020-12-09 09:50:43 +0100

    intent: hoeGaatHet data tweak

--- index.js
+++ index.js
@@ -222,6 +222,8 @@ app.intent('get_status_hoeGaatHet', async (conv) => {
         conv.ask("Het is een mooie dag. Ik heb heel veel zon gekregen. Dit was zalig warm! Ik heb ook echt niet veel dorst. Ik hoop dat jouw dag ook zo mooi was.");
     } else if (zon >= 4 && water >= 40 && zuurstof >= 40) {
         conv.ask("De zon voelt fantastisch vandaag. Alleen begin ik wel al een beetje dorst te krijgen.");
+    } else if (zon >= 3 && water >= 40 && zuurstof >= 40) {
+        conv.ask("De zon was wel een beetje verstopt denk ik. Ik mis ze wel een beetje. Ik begin ook wel al een beetje dorst te krijgen.");
     } else if (zon >= 4 && water < 30 && zuurstof >= 40) {
         conv.ask("Het weer was prachtig vandaag, lekker veel zon. Maar kan je mij water geven? Ik heb echt veel dorst.");
     } else if (zon >= 3 && water >= 50 && zuurstof >= 40) {
